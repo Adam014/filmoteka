30626fde54cf2cef24099edd40d080d33ba853bc
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-11 12:54:59 +0100

    Discovering ways to fetch data in Svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let promise = fetch(`https://api.themoviedb.org/3/movie/popular?api_key=6b6f517b5228ea3d3ea85b1649b6a34a&language=en-US&page=1`).then((x) => x.json());
+</script>
+
+{#await promise}
+	<!-- optionally show something while promise is pending -->
+{:then data}
+	<!-- promise was fulfilled -->
+	<pre>
+		{JSON.stringify(data, null, 2)}
+	</pre>
+{:catch error}
+	<!-- optionally show something while promise was rejected -->
+    <h1>{error}</h1>
+{/await}
