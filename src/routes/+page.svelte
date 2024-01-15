5d925ce8d1da142c0d4ff556a5f374cbe881f2aa
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-15 15:57:18 +0100

    Edited the fetching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,6 @@
 <script>
-    let promise = fetch(`https://api.themoviedb.org/3/movie/popular?api_key=6b6f517b5228ea3d3ea85b1649b6a34a&language=en-US&page=1`).then((x) => x.json());
-</script>
+	export let data;
 
-{#await promise}
-	<!-- optionally show something while promise is pending -->
-{:then data}
-	<!-- promise was fulfilled -->
-	<pre>
-		{JSON.stringify(data, null, 2)}
-	</pre>
-{:catch error}
-	<!-- optionally show something while promise was rejected -->
-    <h1>{error}</h1>
-{/await}
+	console.log(data);
+
+</script>
