207f76de14719d20841d98137a390486e08161c8
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-15 16:55:37 +0100

    Added styling and showing now the data on the page

--- src/components/PopularMovies.svelte
+++ src/components/PopularMovies.svelte
@@ -1,4 +1,6 @@
 <script>
+    import MovieCard from "./MovieCard.svelte";
+
     export let movies;
     console.log(movies)
 </script>
@@ -8,6 +10,19 @@
 
 <div class="popular-movies">
     {#each movies as movie (movie.id)}
-        <li>{movie.title}</li>
+        <MovieCard {movie}/>
     {/each}
 </div>
+
+<style>
+    h3{
+        padding: 0 1rem;
+    }
+    .popular-movies{
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        grid-column-gap: 1rem;
+        grid-row-gap: 2rem;
+    }
+
+</style>
