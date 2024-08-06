4c207a44b4ee80cfd60e09c5241e16af0881e973
adam.stadnik <adam.stadnik@seznam.cz>
2024-08-07 00:23:36 +0200

    reflection

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -10,18 +10,17 @@
 	@import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap');
 	h1 {
 		padding: 0 1rem;
-		padding-left: 2.5rem;
 		font-family: 'Shadows Into Light', cursive;
 		font-size: 3rem;
-		color: white;
+		color: #e5e5e5;
 	}
 	.navbar-container{
 		display: flex;
-		justify-content: left;
-		text-decoration: underline;
+		justify-content: center;
 	}
 	a{
 		text-decoration: none;
 		color: black;
+		text-align: center;
 	}
 </style>
