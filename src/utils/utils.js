25aeab184a072eb68d406a486267dbeb5cd1482b
adam.stadnik <adam.stadnik@seznam.cz>
2024-08-10 18:54:13 +0200

    editing getting the right trailer

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,6 @@
-function findVideoByType(videoList, type, keywords = []) {
+function findVideoByType(videoList, type = []) {
     return videoList.find(video => 
         video.type === type &&
-        keywords.some(keyword => video.name.toLowerCase().includes(keyword)) &&
         !video.name.toLowerCase().includes("restricted")
     );
 }
@@ -9,16 +8,14 @@ function findVideoByType(videoList, type, keywords = []) {
 export function getBestAvailableVideo(videoList) {
     // Priority order for search: Official Trailer, Final Trailer, Any Trailer, Teaser, Clip
     const searchCriteria = [
-        { type: "Trailer", keywords: ["official"] },
-        { type: "Trailer", keywords: ["final"] },
-        { type: "Trailer", keywords: ["trailer"] },
+        { type: "Trailer" },
         { type: "Teaser" },
         { type: "Clip" }
     ];
 
     // Iterate through the search criteria to find the best available video
-    for (const { type, keywords } of searchCriteria) {
-        const video = findVideoByType(videoList, type, keywords || [""]);
+    for (const { type } of searchCriteria) {
+        const video = findVideoByType(videoList, type);
         if (video) {
             return video;
         }
