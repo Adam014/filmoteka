cc7d917fae04dcbcc327b98ad010fc9c520020ce
adam.stadnik <adam.stadnik@seznam.cz>
2024-08-22 22:26:13 +0200

    saving detailed films in films_detailed

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,64 +1,69 @@
 import { supabase } from '../utils/db/supabaseClient';
 
 export async function load({ fetch, url }) {
-    // Get the page number from the query parameters (default to 1 if not present)
     const page = parseInt(url.searchParams.get('page')) || 1;
 
-    // Check if the page exists in Supabase
-    let { data: films, error } = await supabase
-        .from('films')
-        .select('*')
-        .eq('page', page);
-
-    if (error) {
-        console.error('Error fetching from Supabase:', error);
-        films = null;
-    }
-
-    // If films data is found in Supabase, return it
-    if (films && films.length > 0) {
-        return {
-            data: { results: films }
-        };
-    }
-
-    // If not found, fetch from TMDB API
-    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=6b6f517b5228ea3d3ea85b1649b6a34a&language=en-US&page=${page}`;
-    const res = await fetch(apiUrl);
-
-    if (res.ok) {
-        const data = await res.json();
-
-        // Store fetched data into Supabase
-        const filmsToInsert = data.results.map((movie) => ({
-            id: movie.id,
-            adult: movie.adult,
-            backdrop_path: movie.backdrop_path,
-            genre_ids: movie.genre_ids,
-            original_language: movie.original_language,
-            original_title: movie.original_title,
-            popularity: movie.popularity,
-            poster_path: movie.poster_path,
-            release_date: movie.release_date,
-            title: movie.title,
-            video: movie.video,
-            vote_average: movie.vote_average,
-            vote_count: movie.vote_count,
-            page: page // Ensure the page number is stored
-        }));
-
-        const { error: insertError } = await supabase
+    try {
+        // Check if the page exists in Supabase
+        let { data: films, error } = await supabase
             .from('films')
-            .insert(filmsToInsert);
+            .select('*')
+            .eq('page', page);
 
-        if (insertError) {
-            console.error('Error inserting into Supabase:', insertError);
+        if (error) {
+            console.error('Error fetching from Supabase:', error);
         }
 
+        if (films && films.length > 0) {
+            return {
+                data: { results: films }
+            };
+        }
+
+        // If not found in Supabase, fetch from TMDB API
+        const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${page}`;
+        const res = await fetch(apiUrl);
+
+        if (res.ok) {
+            const data = await res.json();
+
+            // Insert fetched data into Supabase
+            const filmsToInsert = data.results.map((movie) => ({
+                id: movie.id,
+                adult: movie.adult,
+                backdrop_path: movie.backdrop_path,
+                genre_ids: movie.genre_ids,
+                original_language: movie.original_language,
+                original_title: movie.original_title,
+                popularity: movie.popularity,
+                poster_path: movie.poster_path,
+                release_date: movie.release_date,
+                title: movie.title,
+                video: movie.video,
+                vote_average: movie.vote_average,
+                vote_count: movie.vote_count,
+                page: page
+            }));
+
+            const { error: insertError } = await supabase
+                .from('films')
+                .upsert(filmsToInsert);
+
+            if (insertError) {
+                console.error('Error inserting into Supabase:', insertError);
+            }
+
+            return {
+                data
+            };
+        } else {
+            throw new Error('Failed to load data from TMDB');
+        }
+    } catch (error) {
+        console.error('Error during data fetching process:', error);
         return {
-            data
+            data: null,
+            error: 'Failed to load data'
         };
     }
-
-    throw new Error('Failed to load data');
 }
--- src/routes/movie/[id]/+page.js
+++ src/routes/movie/[id]/+page.js
@@ -1,7 +1,34 @@
+import { supabase } from '../../../utils/db/supabaseClient';
+
 export async function load({ fetch, params }) {
 	const baseUrl = `https://api.themoviedb.org/3/movie/${params.id}`;
 	const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
+	// Check if the movie exists in the Supabase DB
+	const { data: filmData, error: filmError } = await supabase
+		.from('films')
+		.select('*')
+		.eq('id', params.id)
+		.single();
+
+	if (filmData) {
+		// If film exists, fetch the detailed information from the detailed_film table
+		const { data: detailedData, error: detailedError } = await supabase
+			.from('film_detailed')
+			.select('*')
+			.eq('id', params.id)
+			.single();
+
+		if (detailedData) {
+			// Return the data from the DB
+			return {
+				details: detailedData,
+				videos: detailedData.videos || null,
+			};
+		}
+	}
+
+	// If the film is not found in the DB, fetch the data from the external API
 	const detailsUrl = `${baseUrl}?api_key=${apiKey}&language=en-US`;
 	const videosUrl = `${baseUrl}/videos?api_key=${apiKey}&language=en-US`;
 
@@ -12,21 +39,49 @@ export async function load({ fetch, params }) {
 		]);
 
 		if (!detailsResponse.ok || !videosResponse.ok) {
-			// If any of the responses are not OK, throw an error
 			throw new Error('Failed to fetch data');
 		}
 
 		const detailsData = await detailsResponse.json();
 		const videosData = await videosResponse.json();
 
+		console.log(detailsData)
+
+		const { error: insertDetailedFilmError } = await supabase
+			.from('film_detailed')
+			.insert([
+				{
+					id: detailsData.id,
+					created_at: new Date(),
+					belongs_to_collection: detailsData.belongs_to_collection,
+					budget: detailsData.budget,
+					genres: detailsData.genres,
+					homepage: detailsData.homepage,
+					imdb_id: detailsData.imdb_id,
+					original_country: detailsData.production_countries,
+					overview: detailsData.overview,
+					production_companies: detailsData.production_companies,
+					production_countries: detailsData.production_countries, 
+					revenue: detailsData.revenue,
+					runtime: detailsData.runtime,
+					spoken_languages: detailsData.spoken_languages,
+					status: detailsData.status,
+					tagline: detailsData.tagline,
+					videos: videosData.results 
+				},
+			]);
+
+		if (insertDetailedFilmError) {
+			console.error('Error inserting data into Supabase:', insertDetailedFilmError);
+		}
+
+		// Return the fetched data
 		return {
 			details: detailsData,
-			videos: videosData
+			videos: videosData,
 		};
 	} catch (error) {
 		console.error('Error loading movie data:', error);
-
-		// Optionally return some fallback data or handle the error in a way suitable for your application
 		return {
 			details: null,
 			videos: null,
