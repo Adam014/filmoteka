606ef023db05962b7f75bc18a6f534a89404533d
adam.stadnik <adam.stadnik@seznam.cz>
2024-09-20 16:36:24 +0200

    when user is logged out, hamburger is hidden

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -79,22 +79,27 @@
 		{/if}
 	</div>
 
+	{#if currentUser}
+		<label class="hamburger">
+			<input type="checkbox" on:click={toggleMenu} />
+			<svg viewBox="0 0 36 36">
+				<path
+					class="line line-top-bottom"
+					d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+				/>
+				<path class="line" d="M7 16 27 16" />
+			</svg>
+		</label>
+		{:else}
+			<a href="/login">Login</a>
+			<a href="/register">Register</a>
+	{/if}
+
 	<div class="header-container">
 		<a href="/?page=1" on:click={goToFirstPage}>
 			<h1>Filmoteka</h1>
 		</a>
 	</div>
-
-	<label class="hamburger">
-		<input type="checkbox" on:click={toggleMenu} />
-		<svg viewBox="0 0 36 36">
-			<path
-				class="line line-top-bottom"
-				d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
-			/>
-			<path class="line" d="M7 16 27 16" />
-		</svg>
-	</label>
 
 	<div class="nav-menu" style="transform: translateX({menuOpen ? '0%' : '100%'});">
 		{#if currentUser}
