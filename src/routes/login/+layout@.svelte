45418b9e1c492d87d37531e0e9b350c5b07ba5ba
adam.stadnik <adam.stadnik@seznam.cz>
2024-09-23 19:04:42 +0200

    fixes for login

--- src/routes/login/+layout@.svelte
+++ src/routes/login/+layout@.svelte
@@ -21,8 +21,6 @@
     .form-container {
         display: flex;
         width: 100%;
-        height: 100vh;
-        position: relative;
     }
 
     img {
@@ -40,9 +38,6 @@
     }
 
     @media (max-width: 1024px) {
-        .form-container {
-            position: relative;
-        }
 
         img {
             position: absolute;
