416ac901ede269ccc9d661627fdbbf2069047b13
Adam Stádník <adam.stadnik@seznam.cz>
2025-03-20 09:33:31 +0100

    fixing prod

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,24 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	build: {
-		target: 'es2019', // Ensure compatibility
-		rollupOptions: {
-			output: {
-				format: 'esm' // Force ES module output
-			}
-		}
-	},
-	optimizeDeps: {
-		include: ['lodash'] // Pre-bundle lodash
-	}
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  build: {
+    target: 'es2019', // Ensure compatibility
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      output: {
+        format: 'esm' // Force ES module output
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['lodash', 'country-to-iso'] // Pre-bundle these dependencies
+  }
 };
 
 export default config;
